fd6eca11369af3205991cee16b6258b8887cba6c
dark <tanzid@inbox.ru>
2024-01-17 17:56:55 +0600

    CSRF Token Origin updated

--- blood_bank/settings.py
+++ blood_bank/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+CSRF_TRUSTED_ORIGINS = ['https://bindu-blood-bank.onrender.com', 'https://*.127.0.0.1']
 ALLOWED_HOSTS = ['*']
 
 
