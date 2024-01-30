b2e2e3b367d5d4e3ee41d9387ea93af3fe2a1590
dark <tanzid@inbox.ru>
2024-01-30 13:36:46 +0600

    static setting updated

--- blood_bank/settings.py
+++ blood_bank/settings.py
@@ -130,7 +130,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+STATICFILES_DIRS = [BASE_DIR / "static"] # new
+STATIC_ROOT = BASE_DIR / "staticfiles" 
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 # MEDIA_ROOT = BASE_DIR / 'media'
--- blood_bank/urls.py
+++ blood_bank/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
@@ -8,6 +8,6 @@ urlpatterns = [
     path('accounts/', include('account.urls')),
     path('dashboard/', include('history.urls')),
 ]
-
+urlpatterns += staticfiles_urlpatterns()
 admin.site.index_title = "Bindu"
 admin.site.site_header = "Bindu Admin Panel"
