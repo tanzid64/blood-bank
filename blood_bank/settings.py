d1b1101bdb100f9d5a0dc79bd1f561975a9f3233
tanzid64 <tanzid@inbox.ru>
2024-03-04 13:58:37 +0600

    Vercel setting removed, railway setting updated

--- blood_bank/settings.py
+++ blood_bank/settings.py
@@ -6,7 +6,7 @@ import environ
 env = environ.Env()
 environ.Env.read_env()
 import dj_database_url
-
+import os
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -50,6 +50,7 @@ NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
 MIDDLEWARE = [
     
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,6 +138,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = 'media/'
 # MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
@@ -150,3 +153,5 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
