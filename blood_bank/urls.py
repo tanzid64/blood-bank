81c55e28c8042a15db313d8ca33b091b1b335a72
tanzid64 <tanzid@inbox.ru>
2024-03-04 13:24:04 +0600

    Vercel configuration done

--- blood_bank/urls.py
+++ blood_bank/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
@@ -9,5 +11,6 @@ urlpatterns = [
     path('dashboard/', include('history.urls')),
 ]
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 admin.site.index_title = "Bindu"
 admin.site.site_header = "Bindu Admin Panel"
