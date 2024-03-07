c51c40a67374e5d4af6de2fdbf632d63bc8bc936
tanzid64 <tanzid@inbox.ru>
2024-03-07 15:44:39 +0600

    database changed

--- blood_bank/settings.py
+++ blood_bank/settings.py
@@ -84,18 +84,18 @@ WSGI_APPLICATION = 'blood_bank.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-    'default': dj_database_url.config(
-        default=env("DATABASE_LINK"),
-        engine='django_cockroachdb'
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=env("DATABASE_LINK"),
+#         engine='django_cockroachdb'
+#     )
+# }
 #OnRender Postgre database connection
 # DATABASES = {
 #     'default': dj_database_url.config(
